Fix AUPRC calculation

utils/eval_utils.py
@@ -174,8 +174,7 @@ def partial_auprc(precision, recall, thresholds, min_precision=0.8):
         precision_partial = np.insert(precision[start:], 0, min_precision)
         thresholds_partial = thresholds[start:]
         return precision_partial, recall_partial, thresholds_partial
-        
-    return precision, recall, thresholds
+    return [], [], []
 
 def evaluate(
     y_true, 