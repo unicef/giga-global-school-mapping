f494b4ecd7688718e7f8fcdcd56f5c7052427804
itingzon.unicef <itingzon.unicef@rlogin01.cm.cluster>
2024-05-16 05:44:47 -0500

    Dissolve overlapping predictions

--- utils/pred_utils.py
+++ utils/pred_utils.py
@@ -78,7 +78,9 @@ def cam_predict(iso_code, config, data, geotiff_dir, out_file, buffer_size=50):
     results = filter_by_buildings(iso_code, config, results)
     if len(results) > 0:
         results = data_utils._connect_components(results, buffer_size=0)
-        results = results.sort_values("prob", ascending=False).drop_duplicates(["group"])
+        #results = results.sort_values("prob", ascending=False).drop_duplicates(["group"])
+        results = results.dissolve(by='group', aggfunc='max')
+        results["geometry"] = results["geometry"].centroid
         results.to_file(out_file, driver="GPKG")
     return results
 
