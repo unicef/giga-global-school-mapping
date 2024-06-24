6319243dd3bc84927d7e9a775013b262c61fffc5
itingzon.unicef <itingzon.unicef@rlogin01.cm.cluster>
2024-06-24 18:02:07 -0500

    Add src

--- train.sh
+++ train.sh
@@ -3,11 +3,11 @@
 echo -n "Input ISO: "
 read iso
 
-python train_model.py --config="configs/cnn_configs/convnext_small.yaml" --iso=$iso; 
-python train_model.py --config="configs/cnn_configs/convnext_base.yaml" --iso=$iso;
-python train_model.py --config="configs/cnn_configs/convnext_large.yaml" --iso=$iso;
-python train_model.py --config="configs/cnn_configs/vgg16.yaml" --iso=$iso; 
-python train_model.py --config="configs/cnn_configs/resnet50.yaml" --iso=$iso; 
-python train_model.py --config="configs/cnn_configs/resnet50_fmow_rgb_gassl.yaml" --iso=$iso; 
-python train_model.py --config="configs/cnn_configs/xception.yaml" --iso=$iso; 
-python train_model.py --config="configs/cnn_configs/inceptionv3.yaml" --iso=$iso;
+python src/train_model.py --config="configs/cnn_configs/convnext_small.yaml" --iso=$iso; 
+python src/train_model.py --config="configs/cnn_configs/convnext_base.yaml" --iso=$iso;
+python src/train_model.py --config="configs/cnn_configs/convnext_large.yaml" --iso=$iso;
+python src/train_model.py --config="configs/cnn_configs/vgg16.yaml" --iso=$iso; 
+python src/train_model.py --config="configs/cnn_configs/resnet50.yaml" --iso=$iso; 
+python src/train_model.py --config="configs/cnn_configs/resnet50_fmow_rgb_gassl.yaml" --iso=$iso; 
+python src/train_model.py --config="configs/cnn_configs/xception.yaml" --iso=$iso; 
+python src/train_model.py --config="configs/cnn_configs/inceptionv3.yaml" --iso=$iso;
