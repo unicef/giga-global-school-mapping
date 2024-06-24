41c94fc3c150fc68805372a92ba4d1a800b10ff2
itingzon.unicef <itingzon.unicef@rlogin01.cm.cluster>
2024-06-24 17:44:26 -0500

    Rename files

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -9,11 +9,12 @@ from utils import clean_utils
 from utils import config_utils
 
 import warnings
+
 warnings.filterwarnings("ignore")
 warnings.simplefilter("ignore")
 
 
-def main(args):    
+def main(args):
     config_file = os.path.join(os.getcwd(), args.config)
     config = config_utils.load_config(config_file)
 
@@ -21,19 +22,20 @@ def main(args):
         if args.clean_pos:
             clean_utils.clean_data(
                 iso_code,
-                config=config, 
-                name=args.name, 
-                category=config["pos_class"], 
-                sources=args.sources
+                config=config,
+                name=args.name,
+                category=config["pos_class"],
+                sources=args.sources,
             )
-            
+
         if args.clean_neg:
             clean_utils.clean_data(
                 iso_code,
-                config=config, 
-                name=args.name, 
-                category=config["neg_class"], 
-                sources=args.sources
+                config=config,
+                name=args.name,
+                category=config["neg_class"],
+                imb_ratio=args.imb_ratio,
+                sources=args.sources,
             )
 
 
@@ -42,14 +44,21 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Data Cleaning Pipeline")
     parser.add_argument("--config", help="Path to the configuration file")
     parser.add_argument("--name", help="Folder name", default="clean")
-    parser.add_argument("--sources", help="Sources (e.g. unicef, osm, overture)", default=[], nargs='+')
-    parser.add_argument("--clean_pos", help="Clean positive samples", type=str, default="True")
-    parser.add_argument("--clean_neg", help="Clean negative samples", type=str, default="True")
+    parser.add_argument(
+        "--sources", help="Sources (e.g. unicef, osm, overture)", default=[], nargs="+"
+    )
+    parser.add_argument("--imb_ratio", help="Imbalance ratio", type=int, default=1)
+    parser.add_argument(
+        "--clean_pos", help="Clean positive samples", type=str, default="True"
+    )
+    parser.add_argument(
+        "--clean_neg", help="Clean negative samples", type=str, default="True"
+    )
     args = parser.parse_args()
 
     # Convert to boolean data type
     args.clean_pos = bool(eval(args.clean_pos))
     args.clean_neg = bool(eval(args.clean_neg))
     print(args.sources)
-    
+
     main(args)
