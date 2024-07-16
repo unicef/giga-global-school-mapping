703e58e3fc08dfbf455f4f59ef561538ae61ccda
itingzon.unicef <itingzon.unicef@rlogin01.cm.cluster>
2024-07-16 00:40:48 -0500

    Update config and train bash script for regional experiments

--- train.sh
+++ train.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-echo -n "Input ISO: "
-read iso
+#echo -n "Input ISO: "
+#read iso
 
-python src/train_model.py --config="configs/cnn_configs/convnext_small.yaml" --iso=$iso; 
-python src/train_model.py --config="configs/cnn_configs/convnext_base.yaml" --iso=$iso;
-python src/train_model.py --config="configs/cnn_configs/convnext_large.yaml" --iso=$iso;
-python src/train_model.py --config="configs/vit_configs/vit_b_16.yaml" --iso=$iso; 
-python src/train_model.py --config="configs/vit_configs/vit_l_16.yaml" --iso=$iso; 
-python src/train_model.py --config="configs/vit_configs/vit_h_14.yaml" --iso=$iso; 
-python src/train_model.py --config="configs/vit_configs/swin_v2_t.yaml" --iso=$iso;
-python src/train_model.py --config="configs/vit_configs/swin_v2_s.yaml" --iso=$iso;
-python src/train_model.py --config="configs/vit_configs/swin_v2_b.yaml" --iso=$iso;
+python src/train_model.py --config="configs/cnn_configs/convnext_small.yaml"; 
+# --iso=$iso; 
+python src/train_model.py --config="configs/cnn_configs/convnext_base.yaml"; 
+#--iso=$iso;
+python src/train_model.py --config="configs/cnn_configs/convnext_large.yaml";
+#--iso=$iso;
+python src/train_model.py --config="configs/vit_configs/vit_b_16.yaml";
+#--iso=$iso; 
+python src/train_model.py --config="configs/vit_configs/vit_l_16.yaml";
+#--iso=$iso; 
+python src/train_model.py --config="configs/vit_configs/vit_h_14.yaml"; 
+#--iso=$iso; 
+python src/train_model.py --config="configs/vit_configs/swin_v2_t.yaml"; 
+#--iso=$iso;
+python src/train_model.py --config="configs/vit_configs/swin_v2_s.yaml"; 
+#--iso=$iso;
+python src/train_model.py --config="configs/vit_configs/swin_v2_b.yaml"; 
+#--iso=$iso;
