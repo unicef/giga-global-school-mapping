ca419d6dfacd844572bc42df95347c8a93dae93f
Ubuntu <issatingzon@gmail.com>
2024-11-11 08:48:40 +0000

    Remove lr finder for convnexts

--- train.sh
+++ train.sh
@@ -3,9 +3,9 @@
 echo -n "Input ISO: "
 read iso
 
-python src/train_model.py --config="configs/cnn_configs/convnext_small.yaml" --iso=$iso; 
-python src/train_model.py --config="configs/cnn_configs/convnext_base.yaml" --iso=$iso;
-python src/train_model.py --config="configs/cnn_configs/convnext_large.yaml" --iso=$iso;
+python src/train_model.py --config="configs/cnn_configs/convnext_small.yaml" --iso=$iso --lr_finder=False; 
+python src/train_model.py --config="configs/cnn_configs/convnext_base.yaml" --iso=$iso --lr_finder=False;
+python src/train_model.py --config="configs/cnn_configs/convnext_large.yaml" --iso=$iso --lr_finder=False;
 python src/train_model.py --config="configs/vit_configs/vit_b_16.yaml" --iso=$iso; 
 python src/train_model.py --config="configs/vit_configs/vit_l_16.yaml" --iso=$iso; 
 python src/train_model.py --config="configs/vit_configs/vit_h_14.yaml" --iso=$iso; 
